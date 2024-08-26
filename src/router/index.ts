9e180cdcce2a6b945f304084a7e88b4e889efa7d
Joao Muniz <joao.muniz@sesatech.com.br>
2024-08-26 01:41:14 -0300

    feat: add ingredients route

--- src/router/index.ts
+++ src/router/index.ts
@@ -20,28 +20,14 @@ const router = createRouter({
           name: 'dashboard',
           component: () => import('@/pages/index.vue'),
         },
+        {
+          path: '/ingredients',
+          name: 'ingredients',
+          component: () => import('@/pages/ingredients.vue'),
+        },
       ],
     },
   ],
-})
-
-// Workaround for https://github.com/vitejs/vite/issues/11804
-router.onError((err, to) => {
-  if (err?.message?.includes?.('Failed to fetch dynamically imported module')) {
-    if (!localStorage.getItem('vuetify:dynamic-reload')) {
-      console.log('Reloading page to fix dynamic import error')
-      localStorage.setItem('vuetify:dynamic-reload', 'true')
-      location.assign(to.fullPath)
-    } else {
-      console.error('Dynamic import error, reloading page did not fix it', err)
-    }
-  } else {
-    console.error(err)
-  }
-})
-
-router.isReady().then(() => {
-  localStorage.removeItem('vuetify:dynamic-reload')
 })
 
 export default router
